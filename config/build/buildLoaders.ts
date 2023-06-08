aea92301df0b66e51a51c3aba42bb486771cdf23
Мирослав <myroslav@taxer.ua>
2023-06-08 23:14:47 +0300

    -Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,48 +4,61 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-
   const svgLoader = {
     test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  }
+    use: ["@svgr/webpack"],
+  };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
     use: [
       {
-        loader: 'file-loader',
+        loader: "file-loader",
       },
     ],
-  }
+  };
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            { locales: ["uk", "en"], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
 
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: 'ts-loader',
+    use: "ts-loader",
     exclude: /node_modules/,
-  }
+  };
 
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       {
         loader: "css-loader",
         options: {
           modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:4]' : '[hash:base64:8]'
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:4]"
+              : "[hash:base64:8]",
           },
-        }
+        },
       },
       "sass-loader",
     ],
-  }
+  };
 
-  return [
-    svgLoader,
-    fileLoader,
-    typescriptLoader,
-    cssLoaders
-  ]
+  return [svgLoader, fileLoader, babelLoader, typescriptLoader, cssLoaders];
 }
