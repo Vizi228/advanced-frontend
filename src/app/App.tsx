083ccbee4b1fe694c5ce3d59177679a430dd6517
Мирослав <myroslav@taxer.ua>
2023-06-08 22:47:33 +0300

    -Add refresh plugin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import { FC, Suspense } from "react";
-import { useTranslation } from "react-i18next";
 import "./styles/index.scss";
 
 import { classNames } from "shared/lib/classNames/classNames";
--- src/widgets/LanguageSwitcher/ui/LanguageSwitcher.tsx
+++ src/widgets/LanguageSwitcher/ui/LanguageSwitcher.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
+import { useTranslation } from "react-i18next";
 
 import { classNames } from "shared/lib/classNames/classNames";
 import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 import styles from "./LanguageSwitcher.module.scss";
-import { useTranslation } from "react-i18next";
 
 interface ILanguageSwitcher {
   className?: string;
